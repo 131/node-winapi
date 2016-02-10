5faa60196796aa1a492d3659f4b77e2dc7a7769c
Mejdi Ourir <m.ourir@ivsweb.com>
2016-02-10 15:04:03 +0100

    Proper use nan 1.9

--- src/main.cc
+++ src/main.cc
@@ -1,49 +1,43 @@
 #include <nan.h>
-#include <node.h>
-#include <v8.h>
 #include <windows.h>
 
 
 using namespace v8;
 
-void GetLastInputInfo(const v8::FunctionCallbackInfo<Value>& info) {
-  Isolate* isolate = Isolate::GetCurrent();
-  HandleScope scope(isolate);
 
+
+NAN_METHOD(GetLastInputInfo)
+{
+  NanScope();
+  
   LASTINPUTINFO li;
   li.cbSize = sizeof(LASTINPUTINFO);
   ::GetLastInputInfo(&li);
-
+  
   int elapsed = li.dwTime;
-
-  v8::Local<v8::Number> num = Nan::New(elapsed);
-
-  info.GetReturnValue().Set(num);
+  
+  NanReturnValue(NanNew<v8::Number>(elapsed));
 }
 
-void GetTickCount(const v8::FunctionCallbackInfo<Value>& info) {
-  Isolate* isolate = Isolate::GetCurrent();
-  HandleScope scope(isolate);
-
-
+NAN_METHOD(GetTickCount)
+{
+  NanScope();
+  
   int elapsed = ::GetTickCount();
 
-  v8::Local<v8::Number> num = Nan::New(elapsed);
-
-  info.GetReturnValue().Set(num);
+  NanReturnValue(NanNew<v8::Number>(elapsed));
 }
-
 
 
 
 void Init(Handle<Object> exports) {
-  Isolate* isolate = Isolate::GetCurrent();
-  exports->Set(String::NewFromUtf8(isolate, "GetLastInputInfo"),
-      FunctionTemplate::New(isolate, GetLastInputInfo)->GetFunction());
-
-  exports->Set(String::NewFromUtf8(isolate, "GetTickCount"),
-      FunctionTemplate::New(isolate, GetTickCount)->GetFunction());
-
+  NanScope();
+  
+  exports->Set(NanNew<v8::String>("GetLastInputInfo"), NanNew<v8::FunctionTemplate>(GetLastInputInfo)->GetFunction());
+  exports->Set(NanNew<v8::String>("GetTickCount"), NanNew<v8::FunctionTemplate>(GetTickCount)->GetFunction());
 }
+
+
+
 
 NODE_MODULE(winapi, Init)
