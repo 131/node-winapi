505a32ce3bc27b30f6e5ab4a62c97db45104baf3
François Leurent <fleurent@ivsweb.com>
2018-05-02 15:23:25 +0000

    Stop useless caching of bootime

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 
 # winapi
-Misc natives MS Windows API wrappers for nodejs
+Misc natives MS Windows API wrappers for nodejs & nwjs
 
 [![Version](https://img.shields.io/npm/v/winapi.svg)](https://www.npmjs.com/package/winapi)
 [![License](https://img.shields.io/badge/license-MIT-blue.svg)](http://opensource.org/licenses/MIT)
-![Available platform](https://img.shields.io/badge/platform-win32-blue.svg)
+![Available platform](https://img.shields.io/badge/platform-win32-blue.svg) (https://img.shields.io/badge/platform-nwjs-blue.svg)
 
 
 
--- index.js
+++ index.js
@@ -11,44 +11,39 @@ var winapi;
 const engine = process.versions.nw ? "nwjs" : "node";
 const module_path = `./winapi_${process.platform}_${process.versions.modules}_${engine}.node`;
 try {
-   winapi = require(module_path);
-} catch (e) {
+  winapi = require(module_path);
+} catch(e) {
   throw Error(`Compilation of winapi has failed and there is no pre-compiled binary available for your system. Please install a supported C++11 compiler and reinstall the module 'winapi' (missing ${module_path})`);
 }
 
 
 
-winapi.GetDisplaySettings = function(chain){
-  execFile(winapiCS, ["GetDisplaySettings"], function(err, stdout){
-   chain(err, JSON.parse(stdout));
+winapi.GetDisplaySettings = function(chain) {
+  execFile(winapiCS, ["GetDisplaySettings"], function(err, stdout) {
+    chain(err, JSON.parse(stdout));
   });
-  
-}
+
+};
 
 
 
-winapi.ReOrientDisplay = function(orientation, chain){
+winapi.ReOrientDisplay = function(orientation, chain) {
   execFile(winapiCS, ["ReOrientDisplay", orientation], {}, chain);
-}
+};
 
 
 
-winapi.GetDisplaysList = function( chain){
-  execFile(winapiCS, ["GetDisplaysList"], {}, function(err, stdout){
-   chain(err, JSON.parse(stdout));
+winapi.GetDisplaysList = function(chain) {
+  execFile(winapiCS, ["GetDisplaysList"], {}, function(err, stdout) {
+    chain(err, JSON.parse(stdout));
   });
-}
+};
 
 
 
-var bootTime;
-
-winapi.getIdleTime = function(){
-  if(!bootTime)
-     bootTime = Date.now() - winapi.GetTickCount();
-
- return Date.now() - bootTime - winapi.GetLastInputInfo();
-}
+winapi.getIdleTime = function() {
+  return winapi.GetTickCount() - winapi.GetLastInputInfo();
+};
 
 
 
