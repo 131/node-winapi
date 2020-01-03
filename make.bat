1086439261de16feadee4992b1e408ada48adefc
François Leurent <131.js@cloudyks.org>
2020-01-03 18:30:32 +0100

    with call

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
-npm install
-npm install nw-gyp
+call npm install
+call npm install nw-gyp
 
 set npm_config_runtime=nwjs
 set npm_config_modules=59
-nw-gyp rebuild --target=0.26.6 --arch=ia32 --verbose
+call nw-gyp rebuild --target=0.26.6 --arch=ia32 --verbose
