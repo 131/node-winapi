5908bb70cc9959c7b9ec93b7cc568ab5c67c0be2
François Leurent <131.js@cloudyks.org>
2020-01-03 20:09:55 +0100

    Moar npm tests

--- make.bat
+++ make.bat
@@ -1,6 +1,9 @@
 call npm install
 call npm install nw-gyp
 
-set npm_config_runtime=nwjs
-set npm_config_modules=59
-call nw-gyp rebuild --target=0.26.6 --arch=ia32 --verbose
+call npm run buildnw26
+call npm run buildnw266
+
+dir
+
+
