0a9bef8522ccbe6d64f382e1a779424d31fef780
Francois Leurent <131.code@leurent.email>
2022-04-01 12:04:01 +0200

    Use github actions

--- make.bat
+++ make.bat
@@ -1,6 +1,12 @@
 call npm install
-call npm install nw-gyp
+call npm install -g nw-gyp prebuildify node-gyp
 
+"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
+
+set path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin
+set path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin
+
+where msbuild.exe
 
 REM in vs2017 cmd line
 call prebuildify --target node-webkit@0.14.6 --strip --arch=ia32 --node-gyp=nw-gyp.cmd
