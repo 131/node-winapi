0dc369e1bf8f3987b10048e1a78ca1fe2b83c255
Francois Leurent <131.code@leurent.email>
2022-05-16 16:24:38 +0200

    Hide winapics window

--- index.js
+++ index.js
@@ -6,19 +6,23 @@ var sprintf = require('util').format;
 
 var winapi;
 
+const opts = {
+  windowsHide  : true,
+};
+
 
 var winapiCS = path.join(__dirname, 'WinAPI.exe');
 try {
   winapi = require('node-gyp-build')(__dirname);
 } catch(e) {
   console.log(e);
-  throw Error(sprintf("Compilation of winapi has failed and there is no pre-compiled binary available for your system. Please install a supported C++11 compiler and reinstall the module 'winapi' (missing %s)") );
+  throw Error(sprintf("Compilation of winapi has failed and there is no pre-compiled binary available for your system. Please install a supported C++11 compiler and reinstall the module 'winapi' (missing %s)"));
 }
 
 
 
 winapi.GetDisplaySettings = function(chain) {
-  execFile(winapiCS, ["GetDisplaySettings"], function(err, stdout) {
+  execFile(winapiCS, ["GetDisplaySettings"], opts, function(err, stdout) {
     chain(err, JSON.parse(stdout));
   });
 
@@ -27,28 +31,28 @@ winapi.GetDisplaySettings = function(chain) {
 
 
 winapi.ReOrientDisplay = function(orientation, chain) {
-  execFile(winapiCS, ["ReOrientDisplay", orientation], {}, chain);
+  execFile(winapiCS, ["ReOrientDisplay", orientation], opts, chain);
 };
 
 winapi.MaximizeWindow = function(title, chain) {
-  execFile(winapiCS, ["MaximizeWindow", title], {}, chain);
+  execFile(winapiCS, ["MaximizeWindow", title], opts,  chain);
 };
 
 winapi.MinimizeWindow = function(title, chain) {
-  execFile(winapiCS, ["MinimizeWindow", title], {}, chain);
+  execFile(winapiCS, ["MinimizeWindow", title], opts, chain);
 };
 
 
 winapi.HideWindow = function(title, chain) {
-  execFile(winapiCS, ["HideWindow", title], {}, chain);
+  execFile(winapiCS, ["HideWindow", title], opts, chain);
 };
 
 winapi.ShowWindow = function(title, chain) {
-  execFile(winapiCS, ["ShowWindow", title], {}, chain);
+  execFile(winapiCS, ["ShowWindow", title], opts, chain);
 };
 
 winapi.GetDisplaysList = function(chain) {
-  execFile(winapiCS, ["GetDisplaysList"], {}, function(err, stdout) {
+  execFile(winapiCS, ["GetDisplaysList"], opts, function(err, stdout) {
     chain(err, JSON.parse(stdout));
   });
 };
