90d928985c5de64fc3f289874de39e622baf999c
Francois Leurent <131.code@leurent.email>
2022-09-29 16:20:38 +0200

    New target platform

--- make.bat
+++ make.bat
@@ -9,6 +9,8 @@ REM in vs2017 cmd line
 call prebuildify --target node-webkit@0.14.6 --strip --arch=ia32 --node-gyp=nw-gyp.cmd
 call prebuildify --target node-webkit@0.26.0 --strip --arch=ia32 --node-gyp=nw-gyp.cmd
 call prebuildify --target node-webkit@0.26.6 --strip --arch=ia32 --node-gyp=nw-gyp.cmd
+call prebuildify --target node@10.24.0 --strip --arch=ia32
+
 
 REM in vs2019 cmd line
 call prebuildify --target node@8.17.0 --strip --arch=x64
